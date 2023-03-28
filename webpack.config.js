0183df5bc96e0b8b2d5bcd32222596043b81362b
Lauris Mačāns <85683069+lauriselvijs@users.noreply.github.com>
2023-03-28 14:34:31 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,7 @@ module.exports = {
   devServer: {
     port: 3000,
     open: true,
-    hot: true,
     watchFiles: ["index.html"],
-    compress: true,
-    historyApiFallback: true,
   },
   module: {
     rules: [
